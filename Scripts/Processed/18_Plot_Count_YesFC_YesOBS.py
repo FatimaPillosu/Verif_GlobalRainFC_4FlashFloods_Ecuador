Corrected the file that computes the annual rainfall averages from forecasts.

Scripts/Processed/18_Plot_Count_YesFC_YesOBS.py
@@ -31,115 +31,92 @@ from matplotlib.ticker import MaxNLocator
 # DirOUT (string): relative path of the directory containing the FB values, including the bootstrapped ones.
 
 # INPUT PARAMETERS
+Acc = 12
 DateS = datetime(2020,1,1,0)
 DateF = datetime(2020,12,31,0)
-StepF_Start = 12
-StepF_Final = 120
-Disc_Step = 6
 StepF_2_Plot = 72
-Acc = 12
-EFFCI_list = [6]
+EFFCI = 6
+Max_Count_Plot_yaxis = 40
 MagnitudeInPerc_Rain_Event_FR_list = [85,99]
-RepetitionsBS = 100
-RegionName_list = ["Costa","Sierra"]
+RegionName_list = ["Costa", "Sierra"]
 SystemFC_list = ["ENS", "ecPoint"]
+NumEM_list = [51, 99]
 Colour_SystemFC_list = ["#ec4d37", "#00539CFF"]
-NumEM_list = [51,99]
 Git_repo="/ec/vol/ecpoint_dev/mofp/Papers_2_Write/Verif_Flash_Floods_Ecuador"
 DirIN = "Data/Compute/15_Counts_FC_OBS_Exceeding_VRT"
 DirOUT = "Data/Plot/18_Count_YesFC_YesOBS"
 #####################################################################################################################
 
 
-# Creating the list containing the steps to considered in the computations
-StepF_list = np.arange(StepF_Start, (StepF_Final+1), Disc_Step)
-m = len(StepF_list)
-ind_StepF_2_Plot = np.where(StepF_list == StepF_2_Plot)[0][0]
+# Plotting the counts for a specific VRT
+for MagnitudeInPerc_Rain_Event_FR in MagnitudeInPerc_Rain_Event_FR_list:
 
-# Generating the list of days in the verification period
-TheDates_original = []
-TheDate = DateS
-while TheDate <= DateF:
-      TheDates_original.append(TheDate)
-      TheDate += timedelta(days=1)
-n = len(TheDates_original)
-
-# Computing the counts for a specific EFFCI index
-for EFFCI in EFFCI_list:
+      # Plotting the counts for a specific region
+      for indRegion in range(len(RegionName_list)): 
       
-      # Computing the counts for a specific VRT
-      for MagnitudeInPerc_Rain_Event_FR in MagnitudeInPerc_Rain_Event_FR_list:
+            RegionName = RegionName_list[indRegion]
+            print(" - Plot the count of yes forecasts and observation events for  " + RegionName + ", EFFCI>=" + str(EFFCI) + ", VRT>=tp(" + str(MagnitudeInPerc_Rain_Event_FR) + "th percentile), StepF=" + str(StepF_2_Plot))
+    
+            # Setting the figure where to plot the counts
+            fig, ax1 = plt.subplots(figsize=(12, 8), sharex=True)
 
-            # Computing the counts for a specific region
-            for indRegion in range(len(RegionName_list)): 
-            
-                  # Selecting the region to consider
-                  RegionName = RegionName_list[indRegion]
-
-                  print(" - Plot the count of yes forecasts and observation events for  " + RegionName + ", EFFCI>=" + str(EFFCI) + ", VRT>=tp(" + str(MagnitudeInPerc_Rain_Event_FR) + "th percentile)")
-
-                  # Setting the figure
-                  fig, ax = plt.subplots(2, 1, figsize=(12, 10), sharex=True)
-
-                  # Computing the counts for a specific forecasting system
-                  for indSystemFC in range(len(SystemFC_list)):
-                        
-                        # Selecting the forecasting system to consider and the colour to associate to it in the plots
-                        SystemFC = SystemFC_list[indSystemFC]
-                        Colour_SystemFC = Colour_SystemFC_list[indSystemFC]
-                        NumEM = NumEM_list[indSystemFC]
-
-                        # Initializing the variables containing the FB values, and the bootstrapped ones
-                        tot_count_yes_fc = np.zeros([m,n+1])
-                        tot_count_yes_obs = np.zeros([m,n+1])
-                        
-                        # Computing FB for a specific lead time
-                        for indStepF in range(len(StepF_list)):
-                              
-                              # Selecting the StepF to consider
-                              StepF = StepF_list[indStepF]
-                              print("     - Considering " + SystemFC + " and StepF=" + str(StepF) + " ...")
-
-                              # Storing information about the step computed
-                              tot_count_yes_fc[indStepF, 0] = StepF
-                              tot_count_yes_obs[indStepF, 0] = StepF
-
-                              # Computing the counts for the selected dates
-                              for ind_TheDate in range(n):
-                                    TheDate = TheDates_original[ind_TheDate]
-                                    FileIN = Git_repo + "/" + DirIN + "/" + f"{Acc:02d}" + "h/EFFCI" +  f"{EFFCI:02d}" + "/VRT" + f"{MagnitudeInPerc_Rain_Event_FR:02d}" + "/" + f"{StepF:03d}" + "/" + SystemFC + "/" + RegionName + "/Count_FC_OBS_" + f"{Acc:02d}" + "h_EFFCI" + f"{EFFCI:02d}" + "_VRT" + f"{MagnitudeInPerc_Rain_Event_FR:02d}" + "_" + SystemFC + "_" + RegionName + "_" + TheDate.strftime("%Y%m%d") + "_" + TheDate.strftime("%H") + "_" + f"{StepF:03d}" + ".npy"
-                                    tot_count_yes_fc[indStepF, ind_TheDate+1] = int(np.load(FileIN)[0] / NumEM)
-                                    tot_count_yes_obs[indStepF, ind_TheDate+1] = np.load(FileIN)[1]
-                                    TheDate += timedelta(days=1)
-
-                        print(tot_count_yes_fc)
-                        
-                        # Plotting the counts
-                        ax[0].plot(TheDates_original, tot_count_yes_fc[ind_StepF_2_Plot,1:], ".-", color=Colour_SystemFC, label=SystemFC, linewidth=1)
+            # Initializing the variable that contains the maximum count in the forecasts
+            max_count_yes_fc_list = []
                   
-                  # Adding metadata to the plot
-                  ax[1].bar(TheDates_original, tot_count_yes_obs[ind_StepF_2_Plot,1:], color="black", label="Flood reports")
-                  fig.suptitle(r"$\bf{Counts\ of\ yes-events\ in\ forecasts\ and\ observations}$" + "\nEFFCI>=" +  str(EFFCI) + ", VRT=tp(" + str(MagnitudeInPerc_Rain_Event_FR) + "th perc), Region=" + RegionName + ", " + str(Acc) + "-hourly period ending at StepF=" + str(StepF_2_Plot), fontsize=16, color="#333333")
-                  ax[0].legend(loc="upper right", fontsize=16)
-                  ax[1].legend(loc="upper right", fontsize=16)
-                  ax[1].set_xlabel("Days", fontsize=16, color="#333333")
-                  ax[1].xaxis.set_major_formatter(DateFormatter("%d-%b-%y"))
-                  ax[1].xaxis.set_major_locator(mdates.MonthLocator(bymonthday=1, interval=1))
-                  ax[1].xaxis.set_major_locator(MaxNLocator(integer=True))
-                  ax[1].xaxis.set_tick_params(labelsize=16, rotation=45, colors="#333333")
-                  ax[0].set_ylabel("Counts", fontsize=16, color="#333333")
-                  ax[0].yaxis.set_major_locator(MaxNLocator(integer=True))
-                  ax[0].yaxis.set_tick_params(labelsize=16, colors="#333333")
-                  ax[1].set_ylabel("Counts", fontsize=16, labelpad=30, color="#333333")
-                  ax[1].yaxis.set_major_locator(MaxNLocator(integer=True))
-                  ax[1].yaxis.set_tick_params(labelsize=16, colors="#333333")
-                  plt.tight_layout()
+            # Plotting the counts for a specific forecasting system
+            for indSystemFC in range(len(SystemFC_list)):
+                        
+                  SystemFC = SystemFC_list[indSystemFC]
+                  Colour_SystemFC = Colour_SystemFC_list[indSystemFC]
+                  NumEM = NumEM_list[indSystemFC]
 
-                  # Saving the plot
-                  DirOUT_temp= Git_repo + "/" + DirOUT + "/" + f"{Acc:02d}" + "h"
-                  FileNameOUT_temp = "Counts_YesFC_YesOBS_" + f"{Acc:02d}" + "h_VRT" + f"{MagnitudeInPerc_Rain_Event_FR:02d}" + "_EFFCI" + f"{EFFCI:02d}" + "_" + RegionName + "_"+ f"{StepF_2_Plot:03d}" + ".jpeg"
-                  if not os.path.exists(DirOUT_temp):
-                        os.makedirs(DirOUT_temp)
-                  plt.savefig(DirOUT_temp + "/" + FileNameOUT_temp)
-                  plt.close() 
-                              
+                  # Initializing the variables containing the considered dates, and the correspondent forecasts and observations counts
+                  count_yes_fc = []
+                  count_yes_obs = []
+                  TheDates_list = []
+
+                  # Reading the counts for the selected dates
+                  TheDate = DateS
+                  while TheDate <= DateF:
+                        TheDates_list.append(TheDate)
+                        FileIN = Git_repo + "/" + DirIN + "/" + f"{Acc:02d}" + "h/EFFCI" +  f"{EFFCI:02d}" + "/VRT" + f"{MagnitudeInPerc_Rain_Event_FR:02d}" + "/" + f"{StepF_2_Plot:03d}" + "/" + SystemFC + "/" + RegionName + "/Count_FC_OBS_" + f"{Acc:02d}" + "h_EFFCI" + f"{EFFCI:02d}" + "_VRT" + f"{MagnitudeInPerc_Rain_Event_FR:02d}" + "_" + SystemFC + "_" + RegionName + "_" + TheDate.strftime("%Y%m%d") + "_" + TheDate.strftime("%H") + "_" + f"{StepF_2_Plot:03d}" + ".npy"
+                        count_yes_fc.append(np.load(FileIN)[0] / NumEM)
+                        count_yes_obs.append(np.load(FileIN)[1])
+                        TheDate += timedelta(days=1)
+
+                  # Computing the max count in the forecasts
+                  max_count_yes_fc_list.append(np.max(np.array(count_yes_fc)))
+
+                  # Plotting the counts
+                  ax1.plot(TheDates_list, count_yes_fc, ".-", color=Colour_SystemFC, label=SystemFC, linewidth=1)
+
+            # Adding metadata to the plot
+            max_count_yes_fc = np.max(np.array(max_count_yes_fc_list))
+            ax1.set_title(r"$\bf{Count\ of\ yes-events\ in\ forecasts\ and\ observations}$" + "\nEFFCI>=" +  str(EFFCI) + ", VRT=tp(" + str(MagnitudeInPerc_Rain_Event_FR) + "th perc), Region=" + RegionName + ", " + str(Acc) + "-hourly period ending at StepF=" + str(StepF_2_Plot),  pad=40, fontsize=16, color="#333333")
+            ax1.set_xlabel("Days", fontsize=16, color="#333333")
+            ax1.xaxis.set_major_formatter(DateFormatter("%d-%b"))
+            ax1.xaxis.set_major_locator(mdates.MonthLocator(bymonthday=1, interval=1))
+            ax1.xaxis.set_tick_params(labelsize=16, rotation=20, colors="#333333")
+            ax1.set_ylabel("Forecasts", fontsize=16, labelpad=10, color="#333333")
+            ax1.set_ylim([0, Max_Count_Plot_yaxis])
+            ax1.yaxis.set_tick_params(labelsize=16, colors="#333333")
+            ax1.legend(loc="upper right",  bbox_to_anchor=(0.5, 1.1), ncol=7, fontsize=20, frameon=False)
+            ax1.yaxis.grid(False)
+            ax1.xaxis.grid(True)
+
+            #ax1.yaxis.tick_right()  # Move y-axis ticks to the right
+            ax2 = ax1.twinx()
+            ax2.bar(TheDates_list, count_yes_obs, color="#333333", label="Flood reports")
+            ax2.invert_yaxis()
+            ax2.set_ylabel("Observations", fontsize=16, labelpad=10, color="#333333")
+            ax2.set_ylim([10, 0])
+            ax2.yaxis.set_tick_params(labelsize=16, colors="#333333")
+            ax2.legend(loc="upper left",  bbox_to_anchor=(0.6, 1.1), fontsize=20, frameon=False)
+            
+            # Saving the plot
+            DirOUT_temp= Git_repo + "/" + DirOUT + "/" + f"{Acc:02d}" + "h"
+            FileNameOUT_temp = "Counts_YesFC_YesOBS_" + f"{Acc:02d}" + "h_VRT" + f"{MagnitudeInPerc_Rain_Event_FR:02d}" + "_EFFCI" + f"{EFFCI:02d}" + "_" + RegionName + "_"+ f"{StepF_2_Plot:03d}" + "MaxY" + str(Max_Count_Plot_yaxis) + ".jpeg"
+            if not os.path.exists(DirOUT_temp):
+                  os.makedirs(DirOUT_temp)
+            plt.savefig(DirOUT_temp + "/" + FileNameOUT_temp)
+            plt.close()                     

Scripts/Processed/19_Compute_AverageYear_RainFC.py
@@ -11,30 +11,28 @@ import metview as mv
 
 # INPUT PARAMETERS DESCRIPTION
 # Acc (number, in hours): rainfall accumulation to consider.
-# DateS (date, in format YYYYMMDD): start day of the period to consider.
-# DateF (date, in format YYYYMMDD): final day of the period to consider.
-# AccPerS_list (list of integers): list of the start times of the accumulation periods to consider.
-# BaseTime (integer, in UTC hours): base time to consider for the forecasts.
+# BaseDateTimeS (date, in format YYYYMMDDHH): start base date and time of the period to consider.
+# BaseDateTimeF (date, in format YYYYMMDDHH): final base date and time of the period to consider.
 # StepF_S (integer, in hours): lead time indicating the end of the first accumulation period to consider.
 # StepF_F (integer, in hours): lead time indicating the end of the last accumulation period to consider.
 # Disc_StepF (integer, in hours): discretization between accumulation periods
+# SystemFC_list (list of strings): list of forecasting systems to consider.
 # RegionName_list (list of strings): list of names for the domain's regions.
 # RegionCode_list (list of integers): codes for the domain's regions to consider. 
-# FileIN_Mask (string): relative path of the file containing the domain's mask.
 # Git_repo (string): repository's local path.
+# FileIN_Mask (string): relative path of the file containing the domain's mask.
 # DirIN (string): relative path containing the raw rainfall forecasts.
 # DirOUT (string): relative path for the plots containing annual average of rainfall from forecasts.
 
 # INPUT PARAMETERS
 Acc = 12
-DateS = datetime(2020,1,1,0)
-DateF = datetime(2020,12,31,0)
-AccPerS_list = [0,12]
-LT_S = 1
-LT_F = 10
+BaseDateTimeS = datetime(2020,1,1,0)
+BaseDateTimeF = datetime(2020,12,31,0)
+StepF_S = 12
+StepF_F = 240
 Disc_StepF = 12
 SystemFC_list = ["ENS", "ecPoint"]
-RegionName_list = ["La Costa", "La Sierra"]
+RegionName_list = ["Costa", "Sierra"]
 RegionCode_list = [1,2]
 Git_repo="/ec/vol/ecpoint_dev/mofp/Papers_2_Write/Verif_Flash_Floods_Ecuador"
 FileIN_Mask = "Data/Raw/Ecuador_Mask_ENS/Mask.grib"
@@ -42,65 +40,63 @@ DirIN = "Data/Raw/FC"
 DirOUT = "Data/Compute/19_AverageYear_RainFC"
 ###################################################################################
 
-# Reading the Mask for the regions in the considered domain
+# Reading the mask for the regions in the considered domain
 mask = mv.values(mv.read(Git_repo + "/" + FileIN_Mask))
 
-# Computing the average rainfall within a period, per region
-for LT_day in np.arange(LT_S,LT_F+1):
+# Creating the variable that stores the lead times to consider
+StepF_list = np.arange(StepF_S,StepF_F+1, Disc_StepF)
+Num_StepF = StepF_list.shape[0]
 
-      for AccPerS in AccPerS_list:
+# Computing the annual rainfall average for a specific region
+for ind_Region in range(len(RegionName_list)):
+      
+      RegionName = RegionName_list[ind_Region]
+      RegionCode = RegionCode_list[ind_Region]
+      ind_mask_region = np.where(mask == RegionCode)[0]
 
-            for SystemFC in SystemFC_list:
+      # Computing the annual rainfall average for a specific forecasting system
+      for SystemFC in SystemFC_list:
 
-                  print("Computing the annual average for " + SystemFC + ", for LT = " + str(LT_day) + " day, and for accumulation period starting at " + str(AccPerS) + " UTC")
-                        
-                  ind_Date = 0
-                  TheDate = DateS
-                  while TheDate <= DateF:
-                        
-                        print("Post-Processing date: " , TheDate)
-                        
-                        # Defining the forecasts to consider
-                        VT_S = TheDate + timedelta(hours = (AccPerS))
-                        VT_F = TheDate + timedelta(hours = (AccPerS + Acc))
-                        BaseDateTime = VT_F - timedelta(hours= (AccPerS + Acc) + ((int(LT_day) -1)*24))
-                        Step2 = int((VT_F - BaseDateTime).total_seconds() / 3600)
-                        Step1 = int(Step2 - Acc)
-                        
-                        # Creating the annual rainfall averages
-                        NumDays = (DateF-DateS).days + 1
-                        NumRegions = len(RegionName_list)
-                        tp_av_year = np.empty((NumDays, NumRegions)) * np.nan
+            # Initializing the variables that will contain the annual rainfall averages
+            tp_av_year = np.empty((Num_StepF,2)) * np.nan
+            
+            # Computing the annual rainfall average for a specific lead time
+            for ind_StepF in range(Num_StepF):
                   
+                  StepF = StepF_list[ind_StepF]
+                  StepS = StepF - Acc
+
+                  # Computing the annual rainfall average for all dates
+                  tp_av_year_temp = []
+                  BaseDateTime = BaseDateTimeS
+                  while BaseDateTime <= BaseDateTimeF:
+                        
+                        print("Computing the annual average for " + SystemFC + " in " + RegionName + ", for StepF=" + str(StepF) + ", for the run on " + BaseDateTime.strftime("%Y%m%d") + " at " + BaseDateTime.strftime("%H") + " UTC")
+                                    
                         # Reading the forecasts
                         if SystemFC == "ENS":
-                              FileIN1 =  Git_repo + "/" + DirIN + "/" + SystemFC + "/" + BaseDateTime.strftime("%Y%m%d%H")  + "/tp_" + BaseDateTime.strftime("%Y%m%d") + "_" + BaseDateTime.strftime("%H") + "_" + f"{Step1:03d}" + ".grib"
-                              FileIN2 =  Git_repo + "/" + DirIN + "/" + SystemFC + "/" + BaseDateTime.strftime("%Y%m%d%H")  + "/tp_" + BaseDateTime.strftime("%Y%m%d") + "_" + BaseDateTime.strftime("%H") + "_" + f"{Step2:03d}" + ".grib"
+                              FileIN1 =  Git_repo + "/" + DirIN + "/" + SystemFC + "/" + BaseDateTime.strftime("%Y%m%d%H")  + "/tp_" + BaseDateTime.strftime("%Y%m%d") + "_" + BaseDateTime.strftime("%H") + "_" + f"{StepS:03d}" + ".grib"
+                              FileIN2 =  Git_repo + "/" + DirIN + "/" + SystemFC + "/" + BaseDateTime.strftime("%Y%m%d%H")  + "/tp_" + BaseDateTime.strftime("%Y%m%d") + "_" + BaseDateTime.strftime("%H") + "_" + f"{StepF:03d}" + ".grib"
                               if os.path.isfile(FileIN1) and os.path.isfile(FileIN2):
                                     tp = mv.values((mv.read(FileIN2) - mv.read(FileIN1)) * 1000)
                         if SystemFC == "ecPoint":
-                              FileIN =  Git_repo + "/" + DirIN + "/" + SystemFC + "/" + BaseDateTime.strftime("%Y%m%d%H")  + "/Pt_BC_PERC_" + f"{Acc:03d}"+ "_" + BaseDateTime.strftime("%Y%m%d") + "_" + BaseDateTime.strftime("%H") + "_" + f"{Step2:03d}" + ".grib"
+                              FileIN =  Git_repo + "/" + DirIN + "/" + SystemFC + "/" + BaseDateTime.strftime("%Y%m%d%H")  + "/Pt_BC_PERC_" + f"{Acc:03d}"+ "_" + BaseDateTime.strftime("%Y%m%d") + "_" + BaseDateTime.strftime("%H") + "_" + f"{StepF:03d}" + ".grib"
                               if os.path.isfile(FileIN):
                                     tp = mv.values(mv.read(FileIN))
 
+                        # Extracting the forecasts for a specific region
                         if len(tp) != 0:
-                        
-                              for ind_Region in range(NumRegions):
-                                    RegionName = RegionName_list[ind_Region]
-                                    RegionCode = RegionCode_list[ind_Region]
-                                    ind_mask_region = np.where(mask == RegionCode)[0]
-                                    tp_av_year[ind_Date, ind_Region] = np.mean(tp[:,ind_mask_region])
-                  
-                        ind_Date = ind_Date + 1
+                              tp_av_year_temp.append(np.mean(tp[:,ind_mask_region]))
 
-                        TheDate += timedelta(days=1)
+                        BaseDateTime += timedelta(days=1)
 
                   # Creating the annual rainfall average
-                  tp_av_year = np.nanmean(tp_av_year, axis=0)
+                  tp_av_year[ind_StepF,0] = StepF
+                  tp_av_year[ind_StepF,1] = np.mean(tp_av_year_temp)
                   
                   # Saving the plot
-                  DirOUT_temp= Git_repo + "/" + DirOUT + "/" + f"{Acc:02d}" + "h"
-                  FileNameOUT_temp = "AverageYear_RainFC" + f"{Acc:02d}" + "h_AccPerS_" + f"{AccPerS:02d}" + "UTC_LT" + f"{LT_day:02d}" + "day_" + SystemFC
+                  DirOUT_temp= Git_repo + "/" + DirOUT + "/" + f"{Acc:02d}" + "h" 
+                  FileNameOUT_temp = "AverageYear_RainFC" + f"{Acc:02d}" + "h_" + SystemFC + "_" + RegionName
                   if not os.path.exists(DirOUT_temp):
                         os.makedirs(DirOUT_temp)
                   np.save(DirOUT_temp + "/" + FileNameOUT_temp, tp_av_year)